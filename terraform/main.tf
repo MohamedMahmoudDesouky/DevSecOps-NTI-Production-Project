47476b1d103d36aa16cb94c0ff7b8343b7f0c270
Mahmoud Yasien <mhmudlll4xm@gmail.com>
2026-02-13 04:06:03 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,14 +51,14 @@ module "redis" {
   kms_key_arn    = module.security.redis_kms_arn
 }
 
-# module "vault" {
-#   source              = "./modules/vault"
-#   project_name        = var.project_name
-#   vpc_id              = module.vpc.vpc_id
-#   subnet_ids          = module.vpc.public_subnets
-#   allowed_cidr_blocks = ["0.0.0.0/0"] # Temporary for setup, will restrict in next step
-#   public_key_path     = var.ssh_public_key_path
-# }
+module "vault" {
+  source              = "./modules/vault"
+  project_name        = var.project_name
+  vpc_id              = module.vpc.vpc_id
+  subnet_ids          = module.vpc.public_subnets
+  allowed_cidr_blocks = ["0.0.0.0/0"] # Temporary for setup, will restrict in next step
+  public_key_path     = var.ssh_public_key_path
+}
 
 module "ecr" {
   source       = "./modules/ecr"
